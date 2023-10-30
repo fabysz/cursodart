83b5d7eb6025f397aa8d6562a1c319c9b294eded
Fabiana Alves <biazita.santo@hotmail.com>
2023-10-30 08:48:17 -0300

    Operadores lógicos

--- bin/cursodart.dart
+++ bin/cursodart.dart
@@ -2,8 +2,37 @@ import 'package:cursodart/cursodart.dart' as cursodart;
 
 void main(List<String> arguments) {
   int n = 10;
-  bool r = n != 10;
+  int n2 = 2;
+  bool r = 10 <= 20 && 10 >= 5;
 
   print(r);
-
 }
+
+// == igual
+// != diferente
+// > maior
+// < menor
+// >= maior igual
+// <= menor igual
+
+/* operadores lógicos
+
+Operador E (And) (&&)
+
+f+f = f
+t+f = f
+f+t = f
+t+t = t
+
+Operador Ou (Or) (||)
+
+f+f = f
+t+f = t
+f+t = t
+t+t = t
+
+Operador não (not) !
+
+Ele inverte os valores
+
+ */
