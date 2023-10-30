96acb9d8b68796e1ebc86760747563d39bad3896
Fabiana Alves <biazita.santo@hotmail.com>
2023-10-30 08:28:26 -0300

    Initial commit

---
+++ bin/cursodart.dart
@@ -0,0 +1,9 @@
+import 'package:cursodart/cursodart.dart' as cursodart;
+
+void main(List<String> arguments) {
+  int n = 10;
+  bool r = n != 10;
+
+  print(r);
+
+}
---
+++ test/cursodart_test.dart
@@ -0,0 +1,8 @@
+import 'package:cursodart/cursodart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
